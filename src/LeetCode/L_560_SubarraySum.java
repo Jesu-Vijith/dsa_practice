b4e220ab10f08164d4e584486b917293536e82a6
Vijith <jesuvijith31@gmail.com>
2025-07-29 17:29:43 +0530

    New files added 29-7-2025

---
+++ src/LeetCode/L_560_SubarraySum.java
@@ -0,0 +1,33 @@
+package LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class L_560_SubarraySum {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> subNum = new HashMap<>();
+        subNum.put(0, 1);
+        int total = 0;
+        int count = 0;
+
+        for (int n:nums) {
+            total+=n;
+            if(subNum.containsKey(total-k)){
+                count+=subNum.get(total-k);
+            }
+
+            subNum.put(total, subNum.getOrDefault(total, 0) + 1);
+        }
+        for(Map.Entry<Integer,Integer>map:subNum.entrySet()){
+            System.out.println(map.getKey()+" : "+map.getValue());
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        L_560_SubarraySum ans=new L_560_SubarraySum();
+        int[]nums={1,2,3};
+        int k=3;
+        System.out.println(ans.subarraySum(nums,k));
+    }
+}
